3d686b984fc939a1877982dfb111fa147340ad99
francescogentile25 <francescogentile250893@gmail.com>
2023-02-02 15:57:49 +0100

    ex finito

--- for.js
+++ for.js
@@ -2,10 +2,10 @@
 // for (let i = 1; i <= 100; i++) {
 //     if (i % 15 === 0) {
 //         console.log("FizzBuzz");
-//     } else if (i % 3 === 0) {
-//         console.log("Fizz");
 //     } else if (i % 5 === 0) {
 //         console.log("Buzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
 //     } else {
 //         console.log(i);
 //     }
@@ -17,11 +17,11 @@ for (let i = 1; i <= 100; i++) {
         case i % 15 === 0:
             console.log("FizzBuzz");
             break;
-        case i % 3 === 0:
-            console.log("Fizz");
-            break;
         case i % 5 === 0:
             console.log("Buzz");
+            break;
+        case i % 3 === 0:
+            console.log("Fizz");
             break;
         default:
             console.log(i);
